bea1b7e8251033724fb46df258ed3f21313cd5b0
jationhuang <jationhuang@tencent.com>
2018-03-09 17:38:35 +0800

    local changed

--- src/lib/codeTemplate.js
+++ src/lib/codeTemplate.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
+
+function getAllEntityNames(){
+    //TODO 获取所有自定义标签的名称
+    let arrEntityNames = [];
+    return arrEntityNames.join(', ');
+}
+
 export default 
 `const $filterNullChildren = (children) => {
 	return children.filter(child => child != null);
 }
 
 export default (data, opt) => {
-	const {View, Text, Image, Navigator, Location} = opt;
+	const {View, Text, Image, Navigator, Location, Picker, ${getAllEntityNames()}} = opt;
 	{{code}};
 }`
 
