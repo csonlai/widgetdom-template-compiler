612fb45c25f0989de110dad1798b472c9881cee9
jationhuang <jationhuang@tencent.com>
2018-03-27 11:31:12 +0800

    add Picker support

--- src/lib/codeTemplate.js
+++ src/lib/codeTemplate.js
@@ -1,19 +1,9 @@
-const fs = require('fs');
-
-function getAllEntityNames(){
-    //TODO 获取所有自定义标签的名称
-    let arrEntityNames = [];
-    return arrEntityNames.join(', ');
-}
-
 export default 
 `const $filterNullChildren = (children) => {
 	return children.filter(child => child != null);
 }
 
 export default (data, opt) => {
-	const {View, Text, Image, Navigator, Location, Picker, ${getAllEntityNames()}} = opt;
+	const {View, Text, Image, Navigator, Location, Picker} = opt;
 	{{code}};
 }`
-
-
